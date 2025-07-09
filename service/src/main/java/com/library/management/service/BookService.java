9f54c653ceb4e163c98b87e9862276f255698d47
“mujahid341” <mujahidsaifuzzma@gmail.com>
2025-07-09 15:30:04 +0530

    Added the methods for books like addBooks , getBookByISBN, getAllBooks, updateBookByISBN , deleteBookByISBN

--- service/src/main/java/com/library/management/service/BookService.java
+++ service/src/main/java/com/library/management/service/BookService.java
@@ -2,6 +2,9 @@ package com.library.management.service;
 import com.library.management.domain.Book;
 
 public interface BookService {
-    public Book addBook(Book book);
-    public Book getBookById(Long id);
+    Book addBook(Book book);
+    Book getBookByIsbn(String ISBN);
+    Book updateBook(String iSBN, Book book);
+    Book deleteBook(String ISBN);
+    Iterable<Book> getAllBooks();
 }
