90b1420dd70ffca449457b6157a0cd072bc7c378
“mujahid341” <mujahidsaifuzzma@gmail.com>
2025-07-09 12:36:52 +0530

    Created BookRepository interface extending JpaRepository

---
+++ repository/src/main/java/com/library/management/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.management.repository;
+
+import com.library.management.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findById(Long id);
+}
