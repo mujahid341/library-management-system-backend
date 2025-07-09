94ad375828510f5d9bb2dbf9d5fa8da1a52f37a8
“mujahid341” <mujahidsaifuzzma@gmail.com>
2025-07-09 12:27:50 +0530

    Created Application.java in the startup module to serve as the entry point of the Library Management System. Configured it with @SpringBootApplication annotation.

---
+++ startup/src/main/java/com/library/management/startup/Application.java
@@ -0,0 +1,21 @@
+package com.library.management.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = {
+        "com.library.management.startup",
+        "com.library.management.repository",
+        "com.library.management.domain",
+        "com.library.management.service",
+        "com.library.management.endpoints"
+})
+@EnableJpaRepositories(basePackages = "com.library.management.repository")
+@EntityScan(basePackages = "com.library.management.domain")  // For Book entity
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
